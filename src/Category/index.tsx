1697844cdaeeb530b24f5f751778973e1295230f
olgakoroleva <olg_a88@mail.ru>
2023-09-16 13:08:30 +0300

    add collarse for Categories and Questions

--- src/Category/index.tsx
+++ src/Category/index.tsx
@@ -1,6 +1,7 @@
-import { FC, ReactNode } from "react";
+import { FC, ReactNode, useState } from "react";
 import styles from "./styles.module.scss";
 import { BigArrowDownIcon } from "../Icons/BigArrowDownIcon";
+import cx from "classnames";
 
 type Props = {
   categoryName: string;
@@ -8,13 +9,21 @@ type Props = {
 };
 
 export const Category: FC<Props> = ({ categoryName, children }) => {
+  const [isCollapsed, setIsCollapsed] = useState(true);
+
+  const handleIconClick = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
   return (
     <div>
-      <div className={styles.category}>
+      <div className={styles.category} onClick={handleIconClick}>
         <p className={styles.text}>{categoryName}</p>
-        <BigArrowDownIcon />
+        <div className={cx(styles.icon, isCollapsed ? styles.activeIcon : "")}>
+          <BigArrowDownIcon />
+        </div>
       </div>
-      {children}
+      {!isCollapsed && children}
     </div>
   );
 };
--- src/Question/index.tsx
+++ src/Question/index.tsx
@@ -1,7 +1,7 @@
-
 import { SmallArrowDownIcon } from "../Icons/SmallArrowDownIcon";
 import styles from "./styles.module.scss";
-import { FC } from "react";
+import { FC, Fragment, useState } from "react";
+import cx from "classnames";
 
 type Props = {
   name: string;
@@ -9,26 +9,38 @@ type Props = {
 };
 
 export const Question: FC<Props> = ({ name, text }) => {
+  const [isCollapsed, setIsCollapsed] = useState(true);
+
+  const handleIconClick = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
   return (
-    <div className={styles.questionWrapper}>
+    <div className={styles.questionWrapper} onClick={handleIconClick}>
       <div className={styles.wrapper}>
         <p className={styles.title}>Вопрос</p>
         <div className={styles.question}>
           <p>{name}</p>
-          <SmallArrowDownIcon/>
+          <div className={cx(styles.icon, isCollapsed ? styles.activeIcon : "")}>
+            <SmallArrowDownIcon />
+          </div>
         </div>
       </div>
-      <div className={styles.wrapper}>
-        <p className={styles.title}>Ответ</p>
-        <p className={styles.answer}>{text}</p>
-      </div>
-      <div className={styles.buttonContainer}>
-        <p className={styles.extraText}>Информация была полезной?</p>
-        <div className={styles.buttonWrapper}>
-          <button className={styles.button}>Да</button>
-          <button className={styles.button}>Нет</button>
-        </div>
-      </div>
+      {!isCollapsed && (
+        <>
+          <div className={styles.wrapper}>
+            <p className={styles.title}>Ответ</p>
+            <p className={styles.answer}>{text}</p>
+          </div>
+          <div className={styles.buttonContainer}>
+            <p className={styles.extraText}>Информация была полезной?</p>
+            <div className={styles.buttonWrapper}>
+              <button className={styles.button}>Да</button>
+              <button className={styles.button}>Нет</button>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
